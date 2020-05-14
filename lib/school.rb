9e097b17198eef6fcc18a7938537fb79a9d4a449
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-14 01:39:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,11 +5,11 @@ class School
    
   def initialize(school)
     @school = school
+    @roster = {}
   end
   
   def roster 
-   @roster= {}  
-  
+   @roster
   end
  
     def add_student(name, grade)
