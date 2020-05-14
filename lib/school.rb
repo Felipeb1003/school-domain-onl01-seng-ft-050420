e57ee3a72ff01bd57343102a968d456171400d4b
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-14 01:07:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :name
+   
+  def initialize(school)
+    @school = school
+  end
+  
+  def roster 
+   @hash= {}  
+  
+  end
+  def add_student(name, grade)
+    @name =name
+    @grade=grade
+      @hash[@grade]= []
+    @hash[@grade] << @name
+   
+    
+  end
+  
+  def grade
+  end
+  
+  def sort 
+  end
+end
